2542353c663af5ee3ef21bacfe5ce05f35c162e2
Mihael <Churin_s@inbox.ru>
2025-08-25 23:40:08 +0200

    fix

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,8 +1,11 @@
+
+import java.util.Arrays;
+
 public class NewClass {
     public static void main(String[] args) {
         int[] a = new int[10];
         for (int i = 0; i < a.length; i++) {
-
+            System.out.println(a[i]);
         }
     }
 }
