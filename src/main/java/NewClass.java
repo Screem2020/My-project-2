c859ff51fba1fec6c022b80f260523931eede93c
unknown <churin_s@inbox.ru>
2025-08-26 00:22:30 +0200

    newFUx

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,11 +1,13 @@
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class NewClass {
     public static void main(String[] args) {
         int[] a = new int[10];
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]); // [jxe lheujt
+            new Random().nextInt();
         }
     }
 }
