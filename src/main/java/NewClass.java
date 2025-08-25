5089d4c127bcc6067e9e08931bd9e909a51e4662
unknown <churin_s@inbox.ru>
2025-08-26 00:08:42 +0200

    fix

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -5,7 +5,7 @@ public class NewClass {
     public static void main(String[] args) {
         int[] a = new int[10];
         for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+            System.out.println(a[i]); // [jxe lheujt
         }
     }
 }
