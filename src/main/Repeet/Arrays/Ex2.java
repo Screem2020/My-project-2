4e830c2db82e9f2648158b1f093dc1be4e4cd0cf
unknown <churin_s@inbox.ru>
2025-09-01 11:50:13 +0200

    gitFIx

---
+++ src/main/Repeet/Arrays/Ex2.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Random;
+
+public class Ex2 {
+    /**
+     * Создать массив из случайных чисел, заполнить его случайными числами.Сделать так что бы минимальных значений
+     * было как минимум 3. Найти последний встречный минимальный элемент.
+     */
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        int minIndex = 0;
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new Random().nextInt(-100, 100);
+            System.out.println(arr[i]);
+            if (arr[i] < minIndex & index < 3) {
+                index++;
+            }
+            if (index == 3) System.out.println("Last minimal num = " + arr[i]);
+            }
+    }
+
+}
