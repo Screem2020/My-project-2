ca18558e30e1b4cd6fac54554c24bd7edd44784d
unknown <churin_s@inbox.ru>
2025-09-05 17:38:44 +0200

    Ex7 new File

---
+++ src/main/Repeet/Arrays/Ex6.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        /**
+         * В одномерном массиве найти сумму элементов, расположенных до первого встречного максимального эелмента
+         * массива
+         */
+        int[] arr = new int[10];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new Random().nextInt(-100, 100);
+        }
+        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        for (int j : arr) {
+            if (j < 0) {
+                sum += j;
+            } else break;
+        }
+        System.out.println("sum " + sum);
+    }
+}
