2612281c5ca8784db8e2693b609be3fc6afc5b6c
Mihael <Churin_s@inbox.ru>
2025-09-08 17:08:33 +0200

    fix Ex8

--- src/main/Repeet/Arrays/Ex8.java
+++ src/main/Repeet/Arrays/Ex8.java
@@ -11,12 +11,17 @@ public class Ex8 {
          * Задан одномерный массив. Сформировать другой одномерный массив из отрицателльных чисел, расположенных
          * между первым встречным максимальным и первым встречным минимальным эелементами исходного массива
          */
-        int[] arr = {-10, 2, -50, -22, 44, 26, -1, 80, -6};
+//        int[] arr = {-10, 2, -50, -22, 44, 26, -1, 80, -6};
+        int[] arr = new int[10];
         int max = arr[0];
         int indexMax = 0;
         int min = arr[0];
         int indexMin = 0;
         int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 20) - 10;
+        }
 //        for (int i = 0; i < arr.length; i++) {
 //            arr[i] = new Random().nextInt(-100, 100);
 //        }
@@ -24,17 +29,18 @@ public class Ex8 {
 //        Arrays.sort(arr);
         int[] arrMin = new int[8];
         System.out.println(Arrays.toString(arr));
+
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < max) {
+            if (arr[i] <= -1 && arr[i] < 0) {
                 max = arr[i];
                 indexMax = i;
             }
-            if (arr[i] > min && arr[i] < 0) {
+            if (arr[i] <= min && arr[i] < 0) {
                 min = arr[i];
                 indexMin = i;
             }
 
-            if (arr[i] >= max & arr[i] < 0) {
+            if (arr[i] < 0) {
                 arrMin[index] = arr[i];
                 index++;
             }
@@ -46,12 +52,9 @@ public class Ex8 {
 //                System.out.println(min);
 //            }
 //        }
-        for (int i = indexMax; i > indexMin; i++) {
-            System.out.println(arrMin[index++] = arr[i]);
-        }
         System.out.println(Arrays.toString(arrMin));
-        System.out.println("max " + indexMax);
-        System.out.println("min " + indexMin);
+        System.out.println("max " + max);
+        System.out.println("min " + min);
 
     }
 }
