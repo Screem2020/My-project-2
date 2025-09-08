6c070bee6e5cd9055e5547de701c1634936422b3
unknown <churin_s@inbox.ru>
2025-09-08 21:52:56 +0200

    new file Ex3

---
+++ src/main/Repeet/Arrays2/src/main/java/Ex3.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        /**
+         * Первый отрецательный элемент массива замаенить суммой эллементов, расположенных после максимального
+         */
+        int[] arr =  new int[10];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 60) - 30;
+        }
+        System.out.println(Arrays.toString(arr));
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (arr[i] < 0) {
+                index = i;
+                break;
+            }
+        }
+        for (int i = 0; i <arr.length; i++) {
+            if (i == index) {
+                arr[i] = sum;
+            }
+        }
+        System.out.println("sum " + sum);
+        System.out.println("index " + index);
+        System.out.println(Arrays.toString(arr));
+    }
+}
